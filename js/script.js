961391b4cb7bf071c4eee1702ce6c50d1247a5c3
DylanBlanco <dylanblanco1198@gmail.com>
2025-11-05 17:07:22 +0100

    creata function createCardJs()

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// selezione del container dove inserire le card
+const containerElement = document.getElementById('container-cards');
+// aggiungi card alla pagina
+createCardJs();
+
+
+// funzione per creare una card
+function createCardJs() {
+    const cardJs = document.createElement('div');
+    cardJs.classList.add('card-js');
+    cardJs.style.backgroundColor = 'lightblue';
+    cardJs.style.border = '1px solid blue';
+    cardJs.style.borderRadius = '8px';
+    cardJs.style.padding = '16px';
+    cardJs.style.margin = '8px';
+    cardJs.style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.1)';
+    containerElement.appendChild(cardJs);
+}
