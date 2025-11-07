c9fd7d90f5a26ae6e65a1239cd25bcfd4e8e959e
DylanBlanco <dylanblanco1198@gmail.com>
2025-11-07 12:51:21 +0100

    corrazione CARD JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+/* ---------------------------------------------------------------------------------------------------
+    CREAZIONE CARD JS
+--------------------------------------------------------------------------------------------------- */
 // selezione del container per inserire le card
 const containerElement = document.getElementById('container-cards');
+// funzione per creare una card
+function createCardJs() {
+    const cardJs = document.createElement('div');
+    cardJs.classList.add('card-js');
+    cardJs.style.backgroundColor = 'lightblue';
+    cardJs.style.border = '1px solid blue';
+    cardJs.style.borderRadius = '8px';
+    cardJs.style.padding = '16px';
+    cardJs.style.margin = '8px';
+    cardJs.style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.1)';
+    containerElement.appendChild(cardJs);
+
+    return cardJs;
+}
+
 
 /* ---------------------------------------------------------------------------------------------------
     TASK1 - calcola la somma di due numeri
@@ -92,19 +110,3 @@ btnAddToy.addEventListener('click', () => {
         });
     }
 });
-
-
-// funzione per creare una card
-function createCardJs() {
-    const cardJs = document.createElement('div');
-    cardJs.classList.add('card-js');
-    cardJs.style.backgroundColor = 'lightblue';
-    cardJs.style.border = '1px solid blue';
-    cardJs.style.borderRadius = '8px';
-    cardJs.style.padding = '16px';
-    cardJs.style.margin = '8px';
-    cardJs.style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.1)';
-    containerElement.appendChild(cardJs);
-
-    return cardJs;
-}
